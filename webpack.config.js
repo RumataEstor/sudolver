0bdca1bd69316b1ed89908f3035eae770b8a5419
Dmitry Belyaev <be.dmitry@gmail.com>
2018-09-26 00:44:54 +1000

    More needed stuff

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: [".ts"],
-    }
+        extensions: [".ts", ".js"],
+    },
+    devServer: {
+             contentBase: './dist'
+           },
 };
